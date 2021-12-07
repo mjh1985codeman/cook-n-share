187e538ac5616720d9f231c9a57a722561b9152c
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-12-07 17:38:46 -0600

    server up and running, login/adduser successful w/ graphql

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String
+    email: String
+  }
+  type Query {
+    me: User
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(username: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
