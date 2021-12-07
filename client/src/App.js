e580bf4c17da545a50559ab24d0dfe7760f503e0
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-12-06 20:17:03 -0600

    began component work

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -22,9 +24,10 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <>
-          <Navbar />
           <Switch>
             <Route exact path="/" component={Navbar} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/SignUp" component={SignUp} />
             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
           </Switch>
         </>
