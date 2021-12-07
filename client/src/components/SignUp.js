13ae68272bc111fb9d04321057c859ca485ac217
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-12-06 23:16:53 -0600

    added NavBar component to the other components as well.

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const SignUp = () => {
   return (
     <>
+      <Navbar></Navbar>
       <h1>This is the SignUp Page!</h1>
     </>
   );
