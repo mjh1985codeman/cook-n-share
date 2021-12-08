af4becfe57916c0657580c13a623cbc53e8ef03d
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-12-08 08:41:36 -0600

    added logo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import logo from "../assets/Cook-N-Share.png";
 
 const Home = () => {
   return (
     <>
       <Navbar></Navbar>
+      <img src={logo} alt="Logo" />
       <h1>This is the Home Page!</h1>
     </>
   );
