232fe4774c2c9bc8a5e785bc1e87e0bb5a395fdf
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-12-08 09:13:48 -0600

    styled logo in navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,64 +2,66 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import Auth from "../utils/auth";
-import logo from "../assets/FinalLogo.png";
+import logo from "../assets/croppedLogo.png";
 
 const AppNavbar = () => {
   // set modal display state
 
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <img src={logo} alt="Logo" />
-        <Container fluid>
-          <Nav className="justify-content-center">
-            <Nav.Item>
-              <Nav.Link as={Link} to="/">
-                Home
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as={Link} to="/Search">
-                Search
-              </Nav.Link>
-            </Nav.Item>
-            {!Auth.loggedIn() ? (
+      <div className="nav-style">
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <img src={logo} alt="Logo" />
+          <Container fluid>
+            <Nav className="justify-content-center">
               <Nav.Item>
-                <Nav.Link as={Link} to="/Login">
-                  Login
+                <Nav.Link as={Link} to="/">
+                  Home
                 </Nav.Link>
               </Nav.Item>
-            ) : null}
-            {!Auth.loggedIn() ? (
               <Nav.Item>
-                <Nav.Link as={Link} to="/SignUp">
-                  Sign Up
+                <Nav.Link as={Link} to="/Search">
+                  Search
                 </Nav.Link>
               </Nav.Item>
-            ) : null}
-            {Auth.loggedIn() ? (
-              <Nav.Item>
-                <Nav.Link as={Link} to="/Creations">
-                  Your Creations
-                </Nav.Link>
-              </Nav.Item>
-            ) : null}
-            {Auth.loggedIn() ? (
-              <Nav.Item>
-                <Nav.Link as={Link} to="/Saved">
-                  Saved Recipes
-                </Nav.Link>
-              </Nav.Item>
-            ) : null}
-            {/*Logout Conditionally Rendered if user is logged in*/}
-            {Auth.loggedIn() ? (
-              <Nav.Item>
-                <Nav.Link onClick={() => Auth.logout()}>Logout</Nav.Link>
-              </Nav.Item>
-            ) : null}
-          </Nav>
-        </Container>
-      </Navbar>
+              {!Auth.loggedIn() ? (
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/Login">
+                    Login
+                  </Nav.Link>
+                </Nav.Item>
+              ) : null}
+              {!Auth.loggedIn() ? (
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/SignUp">
+                    Sign Up
+                  </Nav.Link>
+                </Nav.Item>
+              ) : null}
+              {Auth.loggedIn() ? (
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/Creations">
+                    Your Creations
+                  </Nav.Link>
+                </Nav.Item>
+              ) : null}
+              {Auth.loggedIn() ? (
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/Saved">
+                    Saved Recipes
+                  </Nav.Link>
+                </Nav.Item>
+              ) : null}
+              {/*Logout Conditionally Rendered if user is logged in*/}
+              {Auth.loggedIn() ? (
+                <Nav.Item>
+                  <Nav.Link onClick={() => Auth.logout()}>Logout</Nav.Link>
+                </Nav.Item>
+              ) : null}
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
     </>
   );
 };
