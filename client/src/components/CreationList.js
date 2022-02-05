f6c2bd4e937e63581cc4c48321f2bc6a1b274f6a
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2022-02-04 23:15:38 -0600

    reverted back to the old styling

--- client/src/components/CreationList.js
+++ client/src/components/CreationList.js
@@ -12,52 +12,21 @@ const CreationList = ({ creations }) => {
     return <h3>No Creations Yet</h3>;
   }
 
-  function ContextAwareToggle({ children, eventKey, callback }) {
-    const { activeEventKey } = useContext(AccordionContext);
-
-    const decoratedOnClick = useAccordionButton(
-      eventKey,
-      () => callback && callback(eventKey)
-    );
-
-    const isCurrentEventKey = activeEventKey === eventKey;
-
-    return (
-      <button
-        type="button"
-        style={{ backgroundColor: isCurrentEventKey ? "pink" : "lavender" }}
-        onClick={decoratedOnClick}
-      >
-        {children}
-      </button>
-    );
-  }
-
   return (
     <div className="creation-list">
       <Container>
         {creations &&
           creations.map((creation) => (
-            <div key={creation._id}>
-              <Accordion defaultActiveKey="1">
-                <Card>
-                  <Card.Header>
-                    <ContextAwareToggle eventKey="0">
-                      <Container>
-                        <h3>{creation.creationTitle}</h3>
-                        {creation.username} created on {creation.createdAt}
-                      </Container>
-                    </ContextAwareToggle>
-                  </Card.Header>
-                  <Accordion.Collapse eventKey="0">
-                    <Card.Body>
-                      <p>{creation.ingredients}</p>
-                      <p>{creation.creationDescription}</p>
-                      <p>{creation.directions}</p>
-                    </Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              </Accordion>
+            <div key={creation._id} className="card mb-3">
+              <div className="card-header">
+                <h3>{creation.creationTitle}</h3>
+                {creation.username} created on {creation.createdAt}
+              </div>
+              <div className="card-body">
+                <p>{creation.ingredients}</p>
+                <p>{creation.creationDescription}</p>
+                <p>{creation.directions}</p>
+              </div>
             </div>
           ))}
       </Container>
